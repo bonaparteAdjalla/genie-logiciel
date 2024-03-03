636ade72d223c627d8409b13a9ee52493bbc92b7
bonaparteAdjalla <156439899+bonaparteAdjalla@users.noreply.github.com>
2024-03-04 00:35:25 +0100

    Add files via upload
    Page d'accueil

---
+++ acceuil.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vente de Lapins en Ligne</title>
+    <link rel="stylesheet" href="style_acceuil.css">
+</head>
+<body>
+    <?php include("nav.php") ?>
+    <div class="container">
+        <div class="image-container">
+            <img src="bobo.jpeg" alt="Image d'accueil">
+        </div>
+        <div class="text-container">
+            <h2>Bienvenue sur notre site de vente de lapins en ligne !</h2>
+            <p>Découvrez notre sélection de lapins de qualité et trouvez celui qui vous convient.</p>
+            <!-- Autres éléments de publicité -->
+        </div>
+    </div>
+   
+    <footer>
+        <!-- Pied de page -->
+    </footer>
+</body>
+</html>
