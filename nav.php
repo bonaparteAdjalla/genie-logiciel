fc0da19ce8ffb983c4bf72cb470d8629ab392e36
bonaparteAdjalla <156439899+bonaparteAdjalla@users.noreply.github.com>
2024-03-04 00:40:07 +0100

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,11 @@
+<header>
+    <h1>Vente de Lapins en Ligne</h1>
+    <nav>
+        <ul>
+            <li><a href="acceuil.php">Accueil</a></li>
+            <li><a href="inscription.php">S'inscrire</a></li>
+            <li><a href="connexion.php">Se Connecter</a></li>
+            <!-- Ajoutez d'autres liens de navigation ici -->
+        </ul>
+    </nav>
+</header>
